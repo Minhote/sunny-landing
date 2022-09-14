d377d0913ae67cec1c9c244360a73ce4430e1952
Erick <erick_icaza_94@hotmail.com>
2022-09-14 09:58:43 -0500

    Frist Section Completed

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 // Toggle BTN
-const toggleBtn = document.querySelector('.main-container__wrap__nav__toggle')
-const ulNav = document.querySelector('.main-container__wrap__nav__list')
+const toggleBtn = document.querySelector('.main-container__wrap__header__nav__toggle')
+const ulNav = document.querySelector('.main-container__wrap__header__nav__list')
 
 toggleBtn.addEventListener('click', () =>{
     toggleBtn.classList.toggle('active')
     ulNav.classList.toggle('active')
 })
+
